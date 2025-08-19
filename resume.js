6c330fe124d602a15a8f317b31a05574310e2ecd
Biocodeit <glencoelho135@gmail.com>
2025-08-19 23:14:11 +0530

    upload files

---
+++ resume.js
@@ -0,0 +1,181 @@
+
+
+Name =document.querySelector("#name")
+Name.textContent= Intro_data.name
+
+IntroMain = document.querySelector("#Intro")
+address= document.createElement("a")
+address.className = "IntroChild"
+address.textContent = Intro_data.address
+phone = document.createElement("a")
+phone.className = "IntroChild"
+phone.textContent = Intro_data.phone
+email = document.createElement("a")
+email.textContent= Intro_data.email
+email.className = "IntroChild"
+email.href = Intro_data.email
+github = document.createElement("a")
+github.className = "IntroChild"
+github.textContent = Intro_data.github
+github.href = Intro_data.github
+address.className = "IntroChild"
+website = document.createElement("a")
+website.textContent = Intro_data.website
+website.className = "IntroChild"
+website.href = Intro_data.website
+linkdin = document.createElement("a")
+linkdin.textContent = Intro_data.linkdin
+linkdin.className = "IntroChild"
+linkdin.href = Intro_data.linkdin
+
+IntroMain.append(address,phone,email,github,website,linkdin)
+
+// education
+const eduMain= document.querySelector('.education')
+const EduDegree = document.createElement("div")
+EduDegree.className="EduDegree"
+const EduSubject = document.createElement("div")
+EduSubject.className="EduSubject"
+
+
+
+const EduStartYR = document.createElement("div")
+EduStartYR.className="start_yr"
+const EduEndYR = document.createElement("div")
+EduEndYR.className= "end_yr"
+
+for(const [key, value] of Object.entries(edu_data)) {
+  cln_EduDegree  = EduDegree.cloneNode()
+  cln_EduDegree.textContent = value.degree
+  cln_EduSubject = EduSubject.cloneNode()
+  cln_EduSubject.textContent = value.subject
+  cln_EduStartYR = EduStartYR.cloneNode()
+  cln_EduStartYR.textContent = value.start
+  cln_EduEndYR   = EduEndYR.cloneNode()
+  cln_EduEndYR.textContent= value.end
+
+  const edu = document.createElement("div")
+  edu.className = "edu"
+
+  const degreeName = document.createElement("div")
+  degreeName.className= "DegreeName"
+
+  const duration = document.createElement("div")
+  duration.className="Duration"
+
+  const degreeNameYR= document.createElement("div")
+  degreeNameYR.className="ForDuration"
+
+  eduMain.append(edu)
+  edu.append(degreeNameYR)
+  degreeNameYR.append(degreeName,duration)
+  degreeName.append(cln_EduDegree,cln_EduSubject)
+  duration.append(cln_EduStartYR, cln_EduEndYR)
+
+  EduBody = document.createElement("div")
+  EduBody.className= "EduBody"
+  EduBody.textContent= value.university
+  edu.append(EduBody)
+}
+
+
+// experience
+const experience = document.querySelector(".exp_main")
+const exp = document.createElement("div")
+exp.className= "Exp"
+const role = document.createElement("div") 
+role.className= "role"
+const company = document.createElement("div") 
+company.className="company"
+const roleINcompany= document.createElement("div")
+roleINcompany.className= "roleINcompany"
+const ExpSRT= document.createElement("div")
+ExpSRT.className= "start_yr"
+const ExpEND= document.createElement("div")
+ExpEND.className= "end_yr"
+const EXp_body = document.createElement("div")
+EXp_body.className = "Exp_body"
+
+for(const [key, value] of Object.entries(exp_data)) {
+  cln_exp = exp.cloneNode()
+  experience.appendChild(cln_exp)
+  cln_role= role.cloneNode()
+  cln_role.textContent= value.role
+  cln_company = company.cloneNode()
+  cln_company.textContent= value.company
+  cln_ExpSRT = ExpSRT.cloneNode()
+  cln_ExpSRT.textContent= value.start
+  cln_ExpEND = ExpEND.cloneNode()
+  cln_ExpEND.textContent = value.end
+  cln_Exp_body = EXp_body.cloneNode()
+  cln_Exp_body.textContent = value.body
+
+  cln_exp.append(cln_role, cln_company, cln_ExpSRT, cln_ExpEND, cln_Exp_body)
+
+  cln_roleINcompany = roleINcompany.cloneNode()
+  cln_role.parentNode.insertBefore(cln_roleINcompany, cln_role)
+  cln_roleINcompany.append(cln_role, cln_company)
+
+  const ExpDuration = document.createElement("div")
+  ExpDuration.className = "Duration"
+
+  cln_ExpSRT.parentNode.insertBefore(ExpDuration, cln_ExpSRT)
+  ExpDuration.append(cln_ExpSRT, cln_ExpEND)
+
+  const ExpRoleDuration = document.createElement("div")
+  ExpRoleDuration.className= "ForDuration"
+  cln_roleINcompany.parentNode.insertBefore(ExpRoleDuration, cln_roleINcompany)
+  ExpRoleDuration.append(cln_roleINcompany, ExpDuration)
+}
+
+
+// skills
+
+const skill_header= document.querySelector(".skills")
+const skill_section = document.createElement("div")
+skill_section.className="skill_section"
+
+skill_header.parentNode.insertBefore(skill_section,skill_header.nextSibling)
+
+for(const [key,value] of Object.entries(skills)) {
+  const each_skill=document.createElement("div")
+  each_skill.className="each_skill"
+  each_skill.innerHTML= `<strong>${key}</strong> : ${value.join(", ")}`
+  skill_section.appendChild(each_skill)
+}
+
+// projects
+const projects_main= document.querySelector(".projects_main")
+const project= document.createElement("div")
+project.className= "project"
+const proj_title= document.createElement("div")
+proj_title.className= "title"
+const proj_tools= document.createElement("div")
+proj_tools.className = "tools"
+const descript = document.createElement("div")
+descript.className ="descript"
+
+for(const [key,value] of Object.entries(project_data)) {
+  const cloned_project =project.cloneNode()
+  projects_main.appendChild(cloned_project)
+  
+  const cln_proj_title = proj_title.cloneNode()
+  cln_proj_title.innerHTML = `<strong>${value.title}</strong>`
+  const cln_proj_tools = proj_tools.cloneNode()
+  cln_proj_tools.innerHTML = `<strong>Tools: </strong>${value.tools}`
+  const cln_descript =descript.cloneNode()
+  cln_descript.innerHTML= value.body
+  cloned_project.append(cln_proj_title, cln_proj_tools, cln_descript) 
+}
+
+// Acheivements
+achMain = document.querySelector(".Acheivement_main")
+for ( let i of achieve_data) {
+  ach = document.createElement("div")
+  ach.className= "acheivement"
+  ach.textContent = i
+  achMain.append(ach)
+}
+
+lan=document.querySelector(".languages")
+lan.textContent= languages.join(", ")
---
+++ test.js
@@ -0,0 +1,104 @@
+
+Intro_data = {name :"Glen Joshwa Coelho",
+            address: "Mangalore",
+            phone : "+91 9901856921",
+            email : "glencoelho135@gmail.com",
+            github : "https://github.com/biocodeit",
+            website : "www.git.com/glen-coelho",
+            linkdin : "https://www.linkedin.com/in/glen-coelho-160aba215"}
+
+            
+// skills
+const skills={"MD simulation": ["GROMACS","Umbrella Sampling","AMBER", "g_mmpbsa", "MMPBSA.py", "AutoDock Tools","AutoDock4","Vina","Pymol","VMD","chimera","chimeraX","Modeller","SwissModeller","AlphaFold","CaverDock","Rosetta","Avogadro"],
+                "coding":["Python","HTML","CSS","JavaScript","Bash","Numpy","Pandas","Jupyter Lab","VS code","MDAnalysis","nglview","logging","argparse","Prolif","Multiprocess","MatplotLib","Seaborn","Selenium"],
+              "Other":["ssh","slurm","bash scripting","micromamaba","conda","miniconda","Git"]}
+                
+const project_data ={"Project1":{title:"Enzyme Engineering & Molecular Simulation Workflow",
+                                 tools:"GROMACS (AMBER/CHARMM force fields), AutoDock Vina, Modeller, VMD, PyMOL, Chimera, Avogadro, MDAnalysis, ORCA",
+                                 body :`Worked on computational projects aimed at enhancing enzyme activity using in silico techniques. 
+                                 Conducted literature reviews to identify key mutations and functional mechanisms
+Built homology models and designed multiple mutants for experimental screening
+Performed post-MD trajectory analysis including RMSD, RMSF, SASA, distance metrics, and MM/PBSA for
+protein-ligand complexes
+Executed umbrella sampling and metadynamics studies for free energy profiling
+Experienced with rigid and flexible docking using AutoDock Vina
+Used GROMACS with both AMBER and CHARMM force fields across multiple simulation campaigns
+Strong practical experience with molecular visualization and editing using VMD, PyMOL, Chimera, and Avogadro` 
+},
+"Project2":{title:"Python Automation for Molecular Dynamics System Setup",
+                                 tools:"Python, GROMACS, MDAnalysis, Modeller, ParmEd, Bash, argparse, logging, JupyterLab, Git, Pandas, Numpy, Nglview, Prolif, Pymol",
+                                 body :`Developed Python-based command-line tools to automate the complete setup of molecular dynamics
+systems, including protein–ligand preparation, mutant modeling, topology merging, solvation, and ion
+addition.
+Designed modular scripts with log-based checkpointing, input file parsing, and batch execution
+support for handling multiple variants efficiently.
+Integrated key scientific libraries such as MDAnalysis, Modeller, and ParmEd with GROMACS workflows,
+emphasizing reproducibility, scalability, and automation in structural biology pipelines.<a href = "https://github.com/biocodeit/MD_WFlow">(https://github.com/biocodeit/MD_WFlow)</a>` 
+},
+"Project3":{title:"Python Automation for Trajectory Analysis and MM/PBSA Calculations",
+                                 tools:"Python, GROMACS, MDAnalysis, argparse, pandas, bash, custom modules, Git",
+                                 body :`Developed a modular Python script to automate post-MD simulation analysis, including RMSD, RMSF,
+distance monitoring, SASA, hydrogen bonding, residue contacts, interaction energy, and MM/PBSA
+free energy calculations.
+Implemented flexible input parsing, auto-checkpointing, and directory handling to streamline multifeature analysis using MDAnalysis and GROMACS outputs.
+Enabled batch execution of complex analyses through structured input files and reusable
+components, improving reproducibility and reducing manual effort.<a href = "https://github.com/biocodeit/my_MD">(https://github.com/biocodeit/my_MD)</a>` 
+},
+"Project4":{title:"Amino Acid Guesser- Interactively Learning Aminio Acids ",
+                                 tools:"HTML, CSS, JavaScript, GitHub, VS code",
+                                 body :`Built an interactive web game to help users
+                                memorize and learn amino acids through a dynamic, quiz-style interface. Designed and implemented the game logic using 
+                                vanilla JavaScript, including random option generation for each round to maximize learning engagement. Integrated 
+                                3Dmol.js for real-time 3D visualization of amino acid structures and incorporated SVG images for clear, scalable visuals. 
+                                Deployed the site via Netlify, ensuring easy access and sharing. Focused on blending educational value with gamified 
+                                features to support biochemistry students and instructors.<a href = "https://github.com/biocodeit/amino-guesser">(https://github.com/biocodeit/amino-guesser)</a>` 
+},
+"Project5":{title:"Resume Builder- Builds a simple Proffesional Resume ",
+                                 tools:"HTML, CSS, JavaScript, GitHub, VS code",
+                                 body :`Developed a web-based tool to generate professional resumes using user-provided JSON data as input. Utilized 
+                                 vanilla JavaScript for dynamic content rendering and scalability, and applied modern CSS techniques for clean, 
+                                 responsive styling. The application streamlines resume creation for personal websites or job applications, supporting 
+                                 customization and quick updates.<a href = "github.com">github.com</a>` 
+}}
+
+edu_data={edu1:
+          {degree : "M.Sc",
+          subject : "Biotechnology",
+          start : "2021",
+          end : "2023",
+          university :"Mangalore University, Manglaore"},
+        edu2: {
+          degree : "B.Sc",
+          subject : "Biotechnology",
+          start : "2018",
+          end : "2021",
+          university :"St. Aloysius College, Manglaore"
+        }}
+
+exp_data= {exp1 :
+          {role:"Reserach Assistant",
+          company : "Quantumzyme",
+          start : "Aug 2024",
+          end : "Present",
+          body: `Part of a team involved in engineering enzymes to
+improve their efficiency using computational
+techniques for industrial biocatalysis, where I used
+MD simulations, homology modeling, and docking.`
+          },
+        exp233 :
+          {role:"Internship",
+          company : "Quantumzyme",
+          start : "Feb 2024",
+          end : "July 2024",
+          body: `Learnt in silico techniques like molecular docking,
+homology modeling, molecular visualization, MD,
+and QM. Also improved my presentation skills and
+actively contributed to live research projects.`
+          }}
+
+achieve_data = ["Ranked 440 in GATE BT 2023",
+                "Qualified TIFR/JGEEBILS Exam",
+                "Scored 85 percentile in CSIR-NET"]
+
+languages= ["English","Hindi","Kannada"]
+
